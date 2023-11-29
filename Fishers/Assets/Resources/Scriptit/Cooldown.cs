04e82c8445487149b04b37c1649b9d6cbea5f5f2
ayazomar <114134013+ayazomar@users.noreply.github.com>
2023-11-29 10:29:35 +0200

    Cooldown ongelle

--- Fishers/Assets/Resources/Scriptit/Cooldown.cs
+++ Fishers/Assets/Resources/Scriptit/Cooldown.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [System.Serializable]
 public class Cooldown
 {
-    [SerializeField] [Tooltip("Kalastuksen cooldown")] private float CooldownTime;
+    [SerializeField] [Tooltip("Kalastuksen cooldown")] public float CooldownTime;
     private float _nextclicktime;
     public bool isCoolingdown => Time.time < _nextclicktime;//kun on kulunut yli cooldownin keston: isCoolingdown = true
     public void StartCooldown() => _nextclicktime = Time.time + CooldownTime; //seuraava klikkaus = aika siitö lähtien kun cooldown aloitettiin + cooldownin kesto
